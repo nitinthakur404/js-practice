be198617bd7e2abdb01a72dc10ae27c7ba6a4524
nitin kumar <nitinthakur404@gmail.com>
2021-02-23 07:34:16 +0530

    make stopwatch

--- slider.js
+++
@@ -1,6 +0,0 @@
-let arr = ['    New folder/1.jpg', 'New folder/2.jpg', 'New folder/3.jpg', 'New folder/4.jpg', 'New folder/5.jpg']
-let test1 = 0
-console.log("not the value in details ")
-let test = document.getElementById("hiid").style.backgroundImage = URL(arr[1])
-
-console.log(test)
