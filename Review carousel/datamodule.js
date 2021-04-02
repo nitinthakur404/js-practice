2a77b525897020897515581acde30ace5c10f419
nitinthakur404 <nitinthakur404@gmail.com>
2021-04-02 12:18:58 +0530

    add review coursoel

---
+++ Review carousel/datamodule.js
@@ -0,0 +1,33 @@
+let dataOfEmploy = [
+    {
+        image: './1.jfif',
+        Name: 'vinod kumar1',
+        job: 'Php developer',
+        jobDetails: `Communicate with stakeholders to understand their requirementsLorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum consequuntur maxime soluta nobis, rerum aperiam pariatur eius, necessitatibus assumenda veritatis officiis dolorum asperiores`
+    },
+    {
+        image: './2.jfif',
+        Name: 'Nitin kumar2',
+        job: 'WEb Developer',
+        jobDetails: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum consequuntur maxime soluta nobis, rerum aperiam pariatur eius, necessitatibus assumenda veritatis officiis dolorum asperiores perspiciatis? Quaerat perferendis doloremque enim maiores neque`
+    },
+    {
+        image: './3.jfif',
+        Name: 'Manish kumar3',
+        job: 'React developer',
+        jobDetails: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum consequuntur maxime soluta nobis, rerum aperiam pariatur eius, necessitatibus assumenda veritatis officiis dolorum asperiores perspiciatis? Quaerat perferendis doloremque enim maiores neque`
+    },
+    {
+        image: '4.jfif',
+        Name: 'vicky Rajput4',
+        job: 'WordPress developer ',
+        jobDetails: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum consequuntur maxime soluta nobis, rerum aperiam pariatur eius, necessitatibus assumenda veritatis officiis dolorum asperiores perspiciatis? Quaerat perferendis doloremque enim maiores neque`
+    },
+    {
+        image: '5.jfif',
+        Name: 'Golu kumar 5',
+        job: 'Aws webservices',
+        jobDetails: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum consequuntur maxime soluta nobis, rerum aperiam pariatur eius, necessitatibus assumenda veritatis officiis dolorum asperiores perspiciatis? Quaerat perferendis doloremque enim maiores neque`
+    },
+]
+export default dataOfEmploy;
---
+++ Review carousel/index.js
@@ -0,0 +1,50 @@
+import dataOfEmploy from './datamodule.js'
+let count = 0
+let card = ""
+
+let btn = document.querySelectorAll('.btn')
+btn.forEach(function (e) {
+
+    e.addEventListener('click', function (e) {
+        let counter = e.currentTarget.classList
+
+        if (counter.contains('left')) {
+            if(count < dataOfEmploy.length-1){
+            console.log('left side')
+            console.log(count)
+            count++
+            }
+            else{
+                count=0
+            }
+
+        }
+        else if (counter.contains('right')) {
+            console.log('Right side')
+            if (count != 0) {
+                console.log(count)
+                count--
+            }
+            else {
+               count=4
+            }
+
+        } showDetails(count)
+    })
+})
+
+
+function showDetails(value) {
+    console.log()
+    card = `<div id="imgSet">
+    <div id="imgSrc2"></div>
+    <img id="imgSrc" src=${dataOfEmploy[value].image}>
+</div>
+<div id="employDetails">
+    <p id="name">${dataOfEmploy[value].Name}</p>
+    <h4 id="occuipation">${dataOfEmploy[value].job}</h4>
+    <p id="workDetails">${dataOfEmploy[value].jobDetails}</p>
+</div>`
+    console.log(value, 'show')
+document.getElementById('container2').innerHTML=card
+}
