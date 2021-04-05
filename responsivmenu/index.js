d9e4f1e2293d8f300da353687558e7f38a8b2beb
nitinthakur404 <nitinthakur404@gmail.com>
2021-04-05 18:53:31 +0530

    responsive menu

---
+++ responsivmenu/index.js
@@ -0,0 +1,33 @@
+let media = window.matchMedia('(max-width:788px )')
+let navbar = document.getElementById('navbar')
+let humbergerMenu = document.getElementById('humbergerMenu')
+let btn = document.getElementById('humbergerMenu')
+let thiredphasehum = document.getElementById('thiredphasehum')
+thiredphasehum.style.display = 'none'
+
+function mediaQueryHandler(e) {
+    if (e.matches) {
+        navbar.style.display = 'none'
+        humbergerMenu.style.display = ''
+        console.log("query match")
+    }
+    else {
+        navbar.style.display = ''
+        humbergerMenu.style.display = 'none'
+    }
+}
+
+btn.addEventListener('click', function () {
+    console.log(btn.classList[1] == "extended")
+    if (btn.classList[1] == "extended") {
+        thiredphasehum.style.display = ''
+    } else {
+        thiredphasehum.style.display = 'none'
+    }
+})
+function myFunction(e) {
+    e.classList.toggle("change");
+    btn.classList.toggle('extended')
+}
+media.addListener(mediaQueryHandler)
+mediaQueryHandler(media)
